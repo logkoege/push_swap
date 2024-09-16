a0fb41e7d500b08b02ff996002beb371f251cc61
Logan Koegele <logkoege@c3r2p4.42nice.fr>
2024-09-16 04:06:52 +0200

    l

--- sort3.c
+++ sort3.c
@@ -6,18 +6,39 @@
 /*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 23:41:38 by logkoege          #+#    #+#             */
-/*   Updated: 2024/09/16 01:53:18 by logkoege         ###   ########.fr       */
+/*   Updated: 2024/09/16 03:49:28 by logkoege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void sort3(t_node *lst)
+void	sort3(t_node *lst)
 {
 	int	i;
 	
 	i = 0;
-	if()
+	if(sort3d(lst))
+		return;
+	else if (ft_min(lst) = lst->data)
+	{
+		sa(lst);
+		ra(lst);
+	}
+	else if (ft_max(lst) = lst->data)
+	{
+		ra(lst);
+		if (!sort3d(lst))
+			sa(lst);
+	}
+	else 
+	{
+		sa(lst)
+		if (!sort3d(lst))
+		{
+			ra(lst);
+			sa(lst);
+		}
+	}
 }
 int	ft_min(t_node *lst)
 {
@@ -59,4 +80,8 @@ int sort3d(t_node *lst)
 		}
 	}
 	return (0);
+}
+void	sort4(t_node *lst)
+{
+	
 }
